0ec0442d2678392e288cf139a8c58da47e4844d0
lilbbusa <bbostergames@gmail.com>
2024-02-01 16:06:10 -0600

    Random spawning on track
    Values are available in inspector

--- CraftwayRacers/Assets/Scripts/SpawnPointManager.cs
+++ CraftwayRacers/Assets/Scripts/SpawnPointManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -10,26 +11,58 @@ public class SpawnPointManager : MonoBehaviour
     [SerializeField]
     private Vector3 spawn;
     [SerializeField]
-    private Vector3 valueX;
-    [SerializeField] 
-    private float valueY;
+    private int value;
     [SerializeField]
-    private float valueZ;
+    private Vector3 where;
+    [SerializeField]
+    private Vector3 value1 = new Vector3(53.69f, 3.87f, -43.87f);
+    [SerializeField]
+    private Vector3 value2 = new Vector3(90.6f, 3.87f, -17.4f);
+    [SerializeField]
+    private Vector3 value3 = new Vector3(88.3f, 3.87f, 24.8f);
+    [SerializeField]
+    private Vector3 value4 = new Vector3(25.8f, 3.87f, 46.8f);
+    [SerializeField]
+    private Vector3 value5 = new Vector3(-17.2f, 3.87f, 46.8f);
+    [SerializeField]
+    private Vector3 value6 = new Vector3(-51.6f, 3.87f, 45.3f);
+    [SerializeField]
+    private Vector3 value7 = new Vector3(-83.8f, 3.87f, 1.7f);
+    [SerializeField]
+    private Vector3 value8 = new Vector3(-63.4f, 3.87f, -41.8f);
 
-    // Start is called before the first frame update
-    void Start()
+    private List<Vector3> spawnPoints = new List<Vector3>();
+
+    [SerializeField]
+    private int count = 8;
+
+    private void Start()
     {
-       // valueX = new Vector3(Random.Range(-12f, 12f), 5.0f, Random.Range(-12f, 12f));
+        spawnPoints.Add(value1);
+        spawnPoints.Add(value2);
+        spawnPoints.Add(value3);
+        spawnPoints.Add(value4);
+        spawnPoints.Add(value5);
+        spawnPoints.Add(value6);
+        spawnPoints.Add(value7);
+        spawnPoints.Add(value8);
     }
 
     // Update is called once per frame
     void Update()
     {
-        valueX = new Vector3(Random.Range(-12f, 12f), 5.0f, Random.Range(-12f, 12f));
+       
     }
 
     public void SpawnEnemy()
     {
-        Instantiate(enemy, valueX, Quaternion.identity);
+        if(count > 0)
+        {
+            value = Random.Range(0, spawnPoints.Count);
+            where = spawnPoints[value];
+            spawnPoints.RemoveAt(value);
+            count--;
+            Instantiate(enemy, where, Quaternion.identity);
+        }
     }
 }
