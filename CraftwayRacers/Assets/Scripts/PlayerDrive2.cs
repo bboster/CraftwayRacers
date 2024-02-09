fe65db4342c8ec35ad007d9ab58a5712153278a7
Caleb Purnell <cpurnell@mail.bradley.edu>
2024-02-09 13:26:50 -0600

    Updatesss

--- CraftwayRacers/Assets/Scripts/PlayerDrive2.cs
+++ CraftwayRacers/Assets/Scripts/PlayerDrive2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class 2PlayerDrive : MonoBehaviour
+public class PlayerDrive2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
