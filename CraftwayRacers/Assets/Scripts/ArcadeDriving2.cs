e7b6699d173359a1edee551a02cfb53b9808b8ce
evroman03 <evroman@mail.bradley.edu>
2024-02-22 23:33:55 -0600

    CarGooder

--- CraftwayRacers/Assets/Scripts/ArcadeDriving2.cs
+++ CraftwayRacers/Assets/Scripts/ArcadeDriving2.cs
@@ -9,7 +9,7 @@ public class ArcadeDriving2 : MonoBehaviour
     public GameObject CenterOfMass;
     public Transform[] SpringMountList = new Transform[4];
     public GameObject[] Wheels = new GameObject[4]; 
-    public float TopSpeed =20f, MaxSuspensionLength = 1f, SpringStrength=10f, SpringDamper=1f, WheelRadius=0.5f, TireGrip=1f, TireMass = 1f, steerValue=0, ACValue=0, EnginePower=10f;
+    public float TopSpeed =20f, MaxSuspensionLength = 1f, SpringStrength=10f, SpringDamper=1f, WheelRadius=0.5f, TireGrip=.5f, TireMass = 1f, steerValue=0, ACValue=0, EnginePower=10f, MaxSteerAngle=30f;
     public RaycastHit[] HitList = new RaycastHit[4];
     public AnimationCurve FrictionCurve, TorqueCurve;
     public Rigidbody CarRb;
@@ -20,11 +20,11 @@ public class ArcadeDriving2 : MonoBehaviour
         PlayerInput.currentActionMap.FindAction("Steer").canceled += ctx => steerValue = 0;
         PlayerInput.currentActionMap.FindAction("Test").performed += ctx => ACValue = ctx.ReadValue<float>();
         PlayerInput.currentActionMap.FindAction("Test").canceled += ctx => ACValue = 0;
-        /*
+        
         if (CenterOfMass == null)
         {
             CenterOfMass = GameObject.Find("CoM");
-        }*/
+        }
         CarRb = GetComponent<Rigidbody>();
     }
     void Update()
@@ -33,8 +33,6 @@ public class ArcadeDriving2 : MonoBehaviour
         for (int i = temp; i < SpringMountList.Length; i++)
         {
             Suspension(SpringMountList[i], i);
-            //SteeringForce(SpringMountList[i], i);
-            //DrivingForce(SpringMountList[i], i);
         }
     }
     void FixedUpdate()
@@ -42,8 +40,7 @@ public class ArcadeDriving2 : MonoBehaviour
         int temp = 0;
         for (int i = temp; i < SpringMountList.Length; i++)
         {
-            //Suspension(SpringMountList[i], i);
-            SteeringForce(SpringMountList[i], i);
+            TractionForce(SpringMountList[i], i);
             DrivingForce(SpringMountList[i], i);
         }
     }
@@ -61,17 +58,21 @@ public class ArcadeDriving2 : MonoBehaviour
         if (IsGrounded(springLoc, springNum))
         {
             Vector3 accelDir = SpringMountList[springNum].forward;
-            if(ACValue > 0.0f)
+            if (springNum == 0 || springNum == 1)
+            {
+                float steeringFactor = steerValue * MaxSteerAngle;
+                accelDir += SpringMountList[springNum].right * steeringFactor;
+            }
+            if (ACValue > 0.0f)
             {
                 float currentSpeed = Vector3.Dot(transform.forward, CarRb.velocity);
                 float normalizedSpeed = Mathf.Clamp01(Mathf.Abs(currentSpeed/TopSpeed));
                 float availableTorque = TorqueCurve.Evaluate(normalizedSpeed) * ACValue;
                 CarRb.AddForceAtPosition(accelDir * (availableTorque * EnginePower), SpringMountList[springNum].transform.position);
             }
-        }
-        
+        }        
     }
-    public void SteeringForce(Transform springLoc, int springNum)
+    public void TractionForce(Transform springLoc, int springNum)
     {
         if (IsGrounded(springLoc, springNum))
         {
