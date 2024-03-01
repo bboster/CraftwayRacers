238aa3d0a67ad59f76365b5d46e56c1fe89554d5
Caleb Purnell <cpurnell@mail.bradley.edu>
2024-02-29 18:41:32 -0600

    SuperCool

--- CraftwayRacers/Assets/Scripts/Jacks.cs
+++ CraftwayRacers/Assets/Scripts/Jacks.cs
@@ -7,32 +7,27 @@ public class Jacks : MonoBehaviour
 {
     private Rigidbody rb;
     public ArcadeDriving2 Speed;
-    public GameObject Player;
     [SerializeField] float CutSpeed; // Player's speed will divide by this number
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Speed = GetComponent<ArcadeDriving2>();
-        Player.GetComponent<ArcadeDriving2>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Speed = GetComponent<ArcadeDriving2>();
-        Player.GetComponent<ArcadeDriving2>();
-    }
+   
 
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Player" && Speed.Shielded == false)
+        if (collision.gameObject.tag == "Player" )
         {
-            Debug.Log("Jack Jack");
-            rb = Player.GetComponent<ArcadeDriving2>().CarRb;
-            Vector3 IncomingForce = rb.velocity;
-            rb.AddForce(IncomingForce / CutSpeed);
-            //Player.GetComponent<ArcadeDriving2>().EnginePower = Player.GetComponent<ArcadeDriving2>().EnginePower / CutSpeed;
+            Speed = collision.GetComponent<ArcadeDriving2>();
+            if(Speed.Shielded == false)
+            {
+                Debug.Log("Jack Jack");
+
+                rb = Speed.CarRb;
+
+                rb.velocity = rb.velocity / 2;
+                // Vector3 IncomingForce = rb.velocity;
+                //rb.AddForce((IncomingForce * -1) / CutSpeed);
+                //Player.GetComponent<ArcadeDriving2>().EnginePower = Player.GetComponent<ArcadeDriving2>().EnginePower / CutSpeed;
+            }
         }
     }
 }
