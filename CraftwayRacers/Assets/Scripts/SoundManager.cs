c0fda31bc2517cf078971d720a464c24b7d45952
JBBateman <jbateman2@mail.bradley.edu>
2024-03-22 15:10:13 -0500

    SoundManager Functionality Extension

--- CraftwayRacers/Assets/Scripts/SoundManager.cs
+++ CraftwayRacers/Assets/Scripts/SoundManager.cs
@@ -43,4 +43,30 @@ public class SoundManager : MonoBehaviour
         Sound s = System.Array.Find(sounds, sounds => sounds.name == name);
         s.source.Stop();
     }
+
+    [Tooltip("Plays sounds in sequence for their full duration.")]
+    public IEnumerator PlaySoundsInSequence(string[] toPlay)
+    {
+        foreach(string i in toPlay)
+        {
+            Sound s = System.Array.Find(sounds, sounds => sounds.name == i);
+            s.source.Play();
+
+            yield return new WaitForSeconds(s.clip.length);
+        }
+    }
+
+    public IEnumerator PlayEngineSound(GameObject car, string name)
+    {
+        Sound s = System.Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Play();
+
+        for(; ; )
+        {
+            //Get the car speed and relate it to the pitch.
+            //s.source.pitch = //car speed relation.
+
+            yield return new WaitForEndOfFrame();
+        }
+    }
 }
