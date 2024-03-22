011457551d3c80a53b5ca9cfeba101a5812b52c1
JBBateman <jbateman2@mail.bradley.edu>
2024-03-22 14:55:19 -0500

    Sound Manager Setup

---
+++ CraftwayRacers/Assets/Scripts/Sound.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    public AudioMixerGroup mixer;
+
+    [Range(0f, 1f), Tooltip("Value must be between 0 and 1.")]
+    public float volume;
+
+    [Range(0f, 3f), Tooltip("Value must be between 0 and 3.")]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+}
--- CraftwayRacers/Assets/Scripts/SoundManager.cs
+++ CraftwayRacers/Assets/Scripts/SoundManager.cs
@@ -4,5 +4,43 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    
+    public float volume;
+    public Sound[] sounds;
+
+    public static SoundManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+
+            s.source.clip = s.clip;
+            s.source.outputAudioMixerGroup = s.mixer;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = System.Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = System.Array.Find(sounds, sounds => sounds.name == name);
+        s.source.Stop();
+    }
 }
